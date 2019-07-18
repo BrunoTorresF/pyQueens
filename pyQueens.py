5637b845babceb153e81e0d1f896ec908e383cbe
BrunoTorresF <bruno.torres.fabila@gmail.com>
2019-07-17 20:58:34 -0500

    feat(solve): implements n-queens
    fixes looping bug.
    Works for N=4 but individual solutions aren't correct

--- pyQueens.py
+++ pyQueens.py
@@ -21,25 +21,30 @@ class Nqueens:
     def place_queen(self, board, current_row):
         """Driver function will place a new queen on each row"""
         if current_row == self.size:
-            print(board)
+            self.print_board(board)
             self.solutions += 1
         else:
             for column in range(self.size):
                 if self.is_valid(board, current_row, column):
                     board[current_row][column] = 1
-                    self.place_queen(board, current_row + 1)
+                    self.place_queens(board, current_row + 1)
+                board[current_row][column] = 0
 
     def is_valid(self, board, row, column):
         """Helper function to validate position for new queen"""
-        print(board)
-        for i in range(row):
-            if board[i][column] == 1:
+        for i in range(len(board)):
+            if board[i][column] == 1 or board[row][i] == 1:
                 return False
-        for k in range(len(board)):
-            for j in range(row):
-                if board[k][j] == 1 and abs(k - j) == abs(row - column):
+        for k in range(row):
+            for j in range(len(board)):
+                if board[k][j] == 1 and abs(j - k) == abs(column - row):
                     return False
         return True
+
+    def print_board(self, board):
+        for row in range(len(board)):
+            print("%s\n" % board[row])
+        print("--------------------\n")
 
 
 def main():
